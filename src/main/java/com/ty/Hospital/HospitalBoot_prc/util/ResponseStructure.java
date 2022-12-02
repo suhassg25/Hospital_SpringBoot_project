0994adaf3e6b84604910365f8b03306d65a2e685
https://github.com/suhassg25 <sgsuhassg@gmail.com>
2022-12-02 16:29:20 +0530

    suhas changes

--- src/main/java/com/ty/Hospital/HospitalBoot_prc/util/ResponseStructure.java
+++ src/main/java/com/ty/Hospital/HospitalBoot_prc/util/ResponseStructure.java
@@ -1,5 +1,28 @@
 package com.ty.Hospital.HospitalBoot_prc.util;
 
-public class ResponseStructure {
+public class ResponseStructure<T> {
+	
+	private int status;
+	private String message;
+private T data;
+public int getStatus() {
+	return status;
+}
+public void setStatus(int status) {
+	this.status = status;
+}
+public String getMessage() {
+	return message;
+}
+public void setMessage(String message) {
+	this.message = message;
+}
+public T getData() {
+	return data;
+}
+public void setData(T data) {
+	this.data = data;
+}
 
+	
 }
