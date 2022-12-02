f729fd92b1f8e05d1b2d7866830c39e1e6b0eacd
lakshminarayanjavadeveloper <lakshminarayanjavadeveloper@gmail.com>
2022-12-02 21:43:24 +0530

    controller layer added,please have a look

---
+++ src/main/java/com/ty/Hospital/HospitalBoot_prc/controller/EncounterController.java
@@ -0,0 +1,48 @@
+package com.ty.Hospital.HospitalBoot_prc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.ty.Hospital.HospitalBoot_prc.dto.Encounter;
+import com.ty.Hospital.HospitalBoot_prc.service.EncounterService;
+import com.ty.Hospital.HospitalBoot_prc.util.ResponseStructure;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@RestController
+@RequestMapping("encounter")
+public class EncounterController {
+	@Autowired
+	private EncounterService EncounterService;
+
+	@ApiOperation(value="Update Encounter", notes="It is used to update Encounter")
+	@ApiResponses(value= {
+			@ApiResponse(code=201,message="created"),
+			@ApiResponse(code=500,message="Internal Server Error"),
+			@ApiResponse(code=404,message="NotFound")})
+	
+	@PutMapping(consumes= {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE}, produces= {MediaType.APPLICATION_JSON_VALUE})
+	
+	public ResponseEntity<ResponseStructure<Encounter>> updateEncounter(@RequestBody Encounter encounter,@RequestParam int id) {
+		return EncounterService.updateEncounter(encounter, id);
+	}
+
+	@ApiOperation(value="Get Encounter", notes="It is used to get Encounter")
+	@ApiResponses(value= {
+			@ApiResponse(code=201,message="created"),
+			@ApiResponse(code=500,message="Internal Server Error"),
+			@ApiResponse(code=404,message="NotFound")})
+	@GetMapping
+	public ResponseEntity<ResponseStructure<Encounter>> findEncounter(@RequestParam int id)
+	{
+		return EncounterService.getEncounter(id);
+
+	}
+}
